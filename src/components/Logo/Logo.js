4c4b614854f3450e34db937421a6891ba2c44534
blytheb <builtbyblythe@gmail.com>
2025-05-07 00:49:04 -1000

    finished logo working on Image link form

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Tilt from 'react-parallax-tilt';
+import brain from './brain.png';
 import './Logo.css';
 
 const Logo = () => {
     return (
-      <div className="ma4 mt0">
-        <Tilt className="br3" shadow-2>
-            <div className="tilt">👀</div>
+        <Tilt className="tilt br3 shadow-3">
+            <div className="pa3">
+                <img style={{paddingTop: '5px'}} src={brain} alt="logo"/>
+            </div>
         </Tilt>
-      </div>  
-    );
+        );
 }
 
 export default Logo
