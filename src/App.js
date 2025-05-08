0002e557a9ac918a5702623dc6545b26972d79e2
blytheb <builtbyblythe@gmail.com>
2025-05-07 15:25:12 -1000

    most components done (form and rank)

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
+      <ParticlesBg className="particles" type="cobweb" bg={true} />
 {/* {      
       
       <FaceRecognition />} */}
