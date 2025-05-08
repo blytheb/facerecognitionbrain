7877a77f9f0b0f417dd6911775dcb822e06a229d
blytheb <builtbyblythe@gmail.com>
2025-05-08 00:00:30 -1000

    used state to store input values on click to console

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {useState} from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -6,12 +7,25 @@ import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg'
 
 function App() {
+  const [input, setInput] = useState('');
+
+  const onInputChange = (event) => {
+    setInput(event.target.value);
+  }
+  
+  const onButtonSubmit = () => {
+    console.log(input);
+  }
+
   return (
     <div className="App">
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm
+        inputChange={onInputChange}
+        buttonSubmit={onButtonSubmit}
+        />
       <ParticlesBg className="particles" type="cobweb" bg={true} />
 {/* {      
       
