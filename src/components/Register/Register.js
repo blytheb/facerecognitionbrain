bb2a18046b721751f3da4bd87296badabea7344e
blytheb <builtbyblythe@gmail.com>
2025-05-19 23:55:26 -1000

    connected to render server

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://facerecognitionbrain-api-m0at.onrender.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
